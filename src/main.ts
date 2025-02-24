b909975b27672e8dcfca5ded1abf0ff65389f770
Dafdaf <81273171+Dffarhn@users.noreply.github.com>
2025-02-24 15:00:54 +0700

    fix: update cookie-parser import to use default import syntax

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/response/error.filter';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import * as compression from 'compression';
 import helmet from 'helmet';
 
